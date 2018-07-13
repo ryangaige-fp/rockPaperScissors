7726b788aaebe13cdf430e5a43f2236ebc2f6f61
Ryan Gaige <Ryan@fencepencil.com>
2018-07-12 19:46:38 -0600

    fist commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// 3 buttons
+//rock
+//paper
+//scissors
+
+//computer make a random choice
+//compare results
+//document.getElementByID('result').innerHTML = "win"
+
+//Stretch Goals
+//play again
+//keep track of # of wins and losses
+// var paper = "paper"
+// var scissors = "scissors"
+
+var choices = ["Rock", "paper", "scissors"];
+
+var randomPick = "";
+var randomPick2 = "";
+function play(pchoice) {
+  randomPick = choices[pchoice];
+  randomPick2 = choices[Math.floor(Math.random() * choices.length)];
+
+  var element = (document.getElementById("player1").innerText = randomPick);
+  var element = (document.getElementById("player2").innerText = randomPick2);
+
+  if (randomPick == randomPick2) {
+    alert("Draw");
+  } else if (randomPick > randomPick2) {
+    alert("player 1 Wins");
+  } else if (randomPick < randomPick2) {
+    alert("player 2 wins");
+  }
+}
+
+console.log(randomPick, randomPick2);
